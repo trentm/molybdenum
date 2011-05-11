477359fdea175b55d71d65549a5b69ed95b02c90
Trent Mick <trentm@gmail.com>
2011-05-10 23:31:26 -0700

    load jquery and local hub.js in pages

---
+++ static/js/hub.js
@@ -0,0 +1,13 @@
+/*
+ * JS for the hub site.
+ *
+ * Presumes that jquery has already been loaded.
+ */
+
+var Hub = (function() {
+  return {
+    ping: function ping() {
+      alert('pong');
+    }
+  };
+})();
