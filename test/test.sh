577dcea56a0048645a8770b50c1e27cb3291d513
Trent Mick <trentm@gmail.com>
2011-06-17 14:23:20 -0700

    fix 'make test' to support redis setup and teardown

---
+++ test/test.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Setup and run the Hub test suite.
+#
+# Usage:
+#   make test
+#
+
+if [ "$DEBUG" != "" ]; then
+    shift;
+    export PS4='${BASH_SOURCE}:${LINENO}: '
+    set -o xtrace
+fi
+set -o errexit
+
+ROOT=$(cd $(dirname $0)/../; pwd)
+TMP=${ROOT}/test/tmp
+DATADIR=${TMP}/data
+
+
+
+
+#---- support functions
+
+function fatal {
+    echo "$(basename $0): error: $1"
+    exit 1
+}
+
+function errexit {
+    [[ $1 -ne 0 ]] || exit 0
+    cleanup
+    fatal "error exit status $1 at line $2"
+}
+
+function cleanup {
+    echo "== cleanup"
+    kill `cat tmp/hub.pid`
+    kill `cat tmp/redis.pid`
+}
+
+
+#---- mainline
+
+trap 'errexit $? $LINENO' EXIT
+
+cd ${ROOT}/test
+
+
+echo "== start redis"
+mkdir -p tmp
+[[ -e tmp/redis.pid ]] && kill `cat tmp/redis.pid` && sleep 1 || true
+$ROOT/deps/redis/src/redis-server $ROOT/test/redis.conf
+
+echo "== start hub"
+rm -rf ${DATADIR}
+mkdir -p ${DATADIR}
+node $ROOT/app.js -c ${ROOT}/test/test.ini > tmp/hub.log 2>&1 &
+while [[ -z "`curl -sS localhost:3334/api/ping 2>/dev/null | grep pong`" ]]; do
+    sleep 1;
+done
+
+echo "== load fixtures"
+(cd tmp && tar xf ${ROOT}/test/fixtures/eol.git.tgz)
+echo '{"repository": {"url": "tmp/eol.git", "name": "eol"}}' \
+    | curl -sS http://localhost:3334/api/repos/eol -X POST -d @- >/dev/null
+sleep 1  # let it clone eol
+	
+echo "== run the test suite"
+NODE_PATH=node_modules node_modules/.bin/nodeunit test.js
+STATUS=$?
+
+
+cleanup
+exit $STATUS
